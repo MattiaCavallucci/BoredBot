8feab77be9c02650eafbc4bdb68a5440f225a1c2
MattiaCavallucci <1cavam77@solent@ac.uk>
2024-12-06 03:18:25 +0100

    add database integration and message handling to Discord client

---
+++ database.py
@@ -0,0 +1,37 @@
+import sqlite3
+from datetime import datetime
+import os
+
+class Database:
+    def __init__(self) -> None:
+        self.conn = sqlite3.connect("messages.db")
+        self.cursor = self.conn.cursor()
+        self.connect()
+
+    def connect(self):
+        self.cursor.execute('''
+        CREATE TABLE IF NOT EXISTS messages (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            author TEXT NOT NULL,
+            message TEXT NOT NULL,
+            channel TEXT NOT NULL,
+            date TEXT NOT NULL
+        )
+        ''')
+        self.conn.commit()
+        print("Connected to database")
+
+    def insert_message(self, author, message, channel):
+        self.cursor.execute("INSERT INTO messages (author, message, channel, date) VALUES (?, ?, ?, ?)", 
+                            (author, message, channel, datetime.now().strftime("%Y/%m/%d alle %H:%M:%S")))
+        self.conn.commit()
+        print("Message inserted")
+
+    def get_messages_by_author(self, author):
+        self.cursor.execute("SELECT * FROM messages WHERE author = ?", (author,))
+        return self.cursor.fetchall()
+    
+    def close(self):
+        if self.conn:
+            self.conn.close()
+            print("Connection closed")
--- main.py
+++ main.py
@@ -1,15 +1,32 @@
 import discord
-import datetime
+from database import Database
+from datetime import datetime
+from dotenv import load_dotenv
 import os
 import random
 import asyncio
+
+load_dotenv()
+database = Database()
+
+token = os.getenv("TOKEN")
 
 class Client(discord.Client):
     async def on_ready(self):
         print(f"logged in as {self.user}")
 
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
+        if message.content == "!read":
+            messages = database.get_messages_by_author(message.author.name)
+            messages_parsed = "\n".join([f"{message[2]} - **su {message[3]}** - **il {message[4]}**" for message in messages])
+            await message.channel.send(f"**Da {message.author.name}** \n{messages_parsed}")
+            return
+        database.insert_message(message.author.name, message.content, message.channel.name)
+
 intents = discord.Intents.default()
 intents.message_content = True
 
 client = Client(intents=intents)
-client.run("MTMxNDM3MTAzOTQyMTEzNjkyNg.G2RvBx.CPH0Q_q4BNgXuJcDamAyTpk-YUgw0FeKtp64tU")
+client.run(token)
