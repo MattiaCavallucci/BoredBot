9fbe1af744ee1a5897f41e1f8c1e151af04c0566
MattiaCavallucci <1cavam77@solent@ac.uk>
2024-12-06 01:09:47 +0100

    implement basic Discord client with on_ready event and message content intent

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
-import discord.py
+import discord
 import datetime
 import os
 import random
+import asyncio
+
+class Client(discord.Client):
+    async def on_ready(self):
+        print(f"logged in as {self.user}")
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = Client(intents=intents)
+client.run("MTMxNDM3MTAzOTQyMTEzNjkyNg.G2RvBx.CPH0Q_q4BNgXuJcDamAyTpk-YUgw0FeKtp64tU")
